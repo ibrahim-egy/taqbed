32eeb7cfe3e628075f5e8ef6505032c1c9a99271
ibrahim-egy <ibrahimtarek.h@gmail.com>
2023-02-02 22:49:58 +0200

    folder

--- src/routes/Register.js
+++ src/routes/Register.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router = express.Router();
-
+const passport = require('passport');
 
 const db = require('../db/mongo');
 const User = db.User
+
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+        done(err, user);
+    });
+});
 
 
 router.route('/')
